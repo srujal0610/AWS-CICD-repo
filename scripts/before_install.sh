f3187554a6ee22ba8ef4386602936f5cefef2373
Being-Reprobate <145685176+Being-Reprobate@users.noreply.github.com>
2024-06-03 15:56:37 +0530

    added before install file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
 
-# Download artifacts from S3
-aws s3 cp s3://bucket-in-aws-demo/artifacts.zip /tmp/artifacts.zip
-unzip -o /tmp/artifacts.zip -d /tmp
-cp /tmp/sample-build /var/www/html
+sudo service nginx stop
+
