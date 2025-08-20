fa50afb2469464569d185ba2c6dc3b58cf5cd6ee
sprajapati <sprajapati@surekhatech.com>
2025-08-20 10:53:19 +0530

    Updated buildspec file with container name

--- public/content.js
+++ public/content.js
@@ -1,2 +1,2 @@
-const dynamicContent = "Congratulations !! You have reached Srujal's site which is served through a seamless, automagic CICD pipeline and is deployed onto AWS ECS service having launch type as EC2 instance !";
+const dynamicContent = "Congratulations !! You have reached Srujal's site which is served through a seamless and automagic CICD pipeline  !";
 document.getElementById("dynamic-content").innerText = dynamicContent;
